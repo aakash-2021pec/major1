05bb152f01a07aea4a8bd1afe5578690b28f6fe4
aakash-2021pec <71126245+aakash-2021pec@users.noreply.github.com>
2020-11-23 13:18:18 +0530

    Add files via upload

---
+++ algorithms-visualiser/src/utils/navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const NavBar = () => (
+    <div className="bg-dark py-2 px-2 text-center">
+        {/* <span>
+            <a
+                className="btn-sm text-decoration-none bg-dark rounded-0 text-light"
+                href="https://github.com/dipeshpatil/algorithms-visualiser"
+            >
+                <i className="fab text-light fa-github"></i>
+                &nbsp; Repo URL
+            </a>
+            <a
+                className="btn-sm text-decoration-none bg-dark rounded-0 text-light"
+                href="https://linkedin.com/in/dipesh-patil"
+            >
+                <i className="fab text-light fa-linkedin"></i>
+                &nbsp; Dipesh Patil
+            </a>
+        </span> */}
+        <span
+         className="btn-sm text-decoration-none bg-dark rounded-0 text-light"
+        >
+            Major Project 
+        </span>
+    </div>
+);
+
+export default NavBar;
