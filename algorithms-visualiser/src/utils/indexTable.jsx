838a4981923fa2e2a946c4f85ac52f6689720b88
manasvi22sharma <sharmamanasvi2208@gmail.com>
2020-11-25 12:12:41 +0530

    intial commit
    intial commit

--- algorithms-visualiser/src/utils/indexTable.jsx
+++ algorithms-visualiser/src/utils/indexTable.jsx
@@ -19,7 +19,7 @@ export default class IndexTable extends React.Component {
 
     render() {
         return (
-            <div className="container mt-2">
+            <div className="container mt-2 bg-white">
                 <div className="text-center text-muted">
                     <p>
                     In mathematics and computer science, an algorithm () is a finite sequence of well-defined, computer-implementable instructions, typically to solve a class of problems or to perform a computation. Algorithms are always unambiguous and are used as specifications for performing calculations, data processing, automated reasoning, and other tasks.
@@ -35,90 +35,121 @@ export default class IndexTable extends React.Component {
                     </p>
                 </div>
                 <center>
-                    <div className="row mt-2">
+                    {/* <div className="row mt-2"> */}
+                    <div className="col mt-2">
                         <div className="col-sm-4 col-6">
                             <NavLink to="/word-search">
+                                {/* <img
+                                    className="img-fluid w-20 shadowB"
+                                    src={WordSearch}
+                                    alt="Word Searching"
+                                /> */}
+                                <p className="text-dark thumb-title h3">
+                                    Word Searching
+                                </p>
                                 <img
                                     className="img-fluid w-20 shadowB"
                                     src={WordSearch}
                                     alt="Word Searching"
                                 />
-                                <p className="text-dark thumb-title">
-                                    Word Searching
-                                </p>
                             </NavLink>
+                            <p> A word search, word find, word seek, word sleuth or mystery word puzzle is a word game that consists of the letters of words placed in a grid, which usually has a rectangular or square shape. The objective of this puzzle is to find and mark all the words hidden inside the box.</p>
                         </div>
                         <div className="col-sm-4 col-6">
                             <NavLink to="/pathfinder">
+                            <p className="text-dark thumb-title h3">
+                                    Path Finding
+                                </p>
                                 <img
                                     className="img-fluid w-20 shadowB"
                                     src={PathThumb}
                                     alt="Path Finding"
                                 />
-                                <p className="text-dark thumb-title">
+                                {/* <p className="text-dark thumb-title">
                                     Path Finding
-                                </p>
+                                </p> */}
                             </NavLink>
+                            <p> defination of word Search to be added</p>
                         </div>
                         <div className="col-sm-4 col-6">
                             <NavLink to="/sorting">
+                            <p className="text-dark thumb-title h3">
+                                    Sorting Visualiser
+                                </p>
                                 <img
                                     className="img-fluid w-20 shadowB"
                                     src={SortingThumb}
                                     alt="Sorting"
                                 />
-                                <p className="text-dark thumb-title">
+                                {/* <p className="text-dark thumb-title">
                                     Sorting Visualiser
-                                </p>
+                                </p> */}
                             </NavLink>
+                            <p> defination of word Search to be added</p>
                         </div>
                         <div className="col-sm-4 col-6">
                             <NavLink to="/n-queens-problem">
+                            <p className="text-dark thumb-title h3">
+                                    NQueens Problem
+                                </p>
                                 <img
                                     className="img-fluid w-20 shadowB"
                                     src={NQueensThumb}
                                     alt="NQueens"
                                 />
-                                <p className="text-dark thumb-title">
+                                {/* <p className="text-dark thumb-title">
                                     NQueens Problem
-                                </p>
+                                </p> */}
                             </NavLink>
+                            <p> defination of word Search to be added</p>
                         </div>
                         <div className="col-sm-4 col-6">
                             <NavLink to="/linear-search">
+                            <p className="text-dark thumb-title h3">
+                                    Linear Search
+                                </p>
                                 <img
                                     className="img-fluid w-20 shadowB"
                                     src={LinearThumb}
                                     alt="Linear Search"
                                 />
-                                <p className="text-dark thumb-title">
+                                {/* <p className="text-dark thumb-title">
                                     Linear Search
-                                </p>
+                                </p> */}
                             </NavLink>
+                            <p> defination of word Search to be added</p>
                         </div>
                         <div className="col-sm-4 col-6">
                             <NavLink to="/binary-search">
+                            <p className="text-dark thumb-title h3">
+                                    Binary Search
+                                </p>
                                 <img
                                     className="img-fluid w-20 shadowB"
                                     src={BinaryThumb}
                                     alt="Binary Search"
                                 />
-                                <p className="text-dark thumb-title">
+                                {/* <p className="text-dark thumb-title">
                                     Binary Search
-                                </p>
+                                </p> */}
                             </NavLink>
+                            <p> defination of word Search to be added</p>
                         </div>
                         <div className="col-sm-4 col-6">
                             <NavLink to="/rat-in-a-maze">
+                            <p className="text-dark thumb-title h3">
+                                    Rat In A Maze
+                                </p>
                                 <img
                                     className="img-fluid w-20 shadowB"
                                     src={RatThumb}
                                     alt="Rat In A Maze"
                                 />
-                                <p className="text-dark thumb-title">
+                                {/* <p className="text-dark thumb-title">
                                     Rat In A Maze
-                                </p>
+                                </p> */}
                             </NavLink>
+                            <p> defination of word Search to be added</p>
                         </div>
                     </div>
                 </center>
